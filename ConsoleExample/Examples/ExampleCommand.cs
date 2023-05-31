5174c0f81df46cf068f7b8e455baeb1856a7a63f
Ilya Samarin <ilia-samarin@bk.ru>
2023-05-31 12:39:17 +0700

    Добавлена ссылка для тестирования webapp

--- ConsoleExample/Examples/ExampleCommand.cs
+++ ConsoleExample/Examples/ExampleCommand.cs
@@ -112,7 +112,7 @@ namespace PRTelegramBot.Examples
             // Создает inline кнопку с ссылкой
             var url = new InlineURL("Google", "https://google.com");
             // Создаем кнопку для работы с webApp
-            var webdata = new InlineWebApp("WebAppp", "https://google.com");
+            var webdata = new InlineWebApp("WebApp test", "https://prethink.github.io/telegram/webapp.html");
 
             //IInlineContent - реализуют все inline кнопки
             List<IInlineContent> menu = new();
