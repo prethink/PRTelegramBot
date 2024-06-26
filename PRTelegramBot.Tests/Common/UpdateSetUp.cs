83e90d241450d37d9ab4801b61ae9c68423f5ee5
PreThink <ilia-samarin@bk.ru>
2024-06-26 08:41:14 +0700

    Еще тесты

--- PRTelegramBot.Tests/Common/UpdateSetUp.cs
+++ PRTelegramBot.Tests/Common/UpdateSetUp.cs
@@ -31,7 +31,383 @@ namespace PRTelegramBot.Tests.Common
 
         #region MessageEvents
 
+        public static Update CreateMessageTypeVenue()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Venue = new();
+            return update;
+        }
 
+        public static Update CreateMessageTypeAnimation()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Animation = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeText()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Text = "Example text";
+            return update;
+        }
+
+        public static Update CreateMessageTypeAudio()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Audio = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeDocument()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Document = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypePhoto()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Photo = new List<PhotoSize> { new PhotoSize() }.ToArray();
+            return update;
+        }
+
+        public static Update CreateMessageTypeSticker()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Sticker = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeStory()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Story = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideo()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Video = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideoNote()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.VideoNote = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVoice()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Voice = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeContact()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Contact = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeDice()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Dice = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGame()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Game = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypePoll()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Poll = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeLocation()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Location = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeNewChatMembers()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.NewChatMembers = new List<User> { new User() }.ToArray();
+            return update;
+        }
+
+        public static Update CreateMessageTypeLeftChatMember()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.LeftChatMember = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeNewChatTitle()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.NewChatTitle = "New Chat Title";
+            return update;
+        }
+
+        public static Update CreateMessageTypeNewChatPhoto()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.NewChatPhoto = new List<PhotoSize> { new PhotoSize() }.ToArray();
+            return update;
+        }
+
+        public static Update CreateMessageTypeDeleteChatPhoto()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.DeleteChatPhoto = true;
+            return update;
+        }
+
+        public static Update CreateMessageTypeGroupChatCreated()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GroupChatCreated = true;
+            return update;
+        }
+
+        public static Update CreateMessageTypeSupergroupChatCreated()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.SupergroupChatCreated = true;
+            return update;
+        }
+
+        public static Update CreateMessageTypeChannelChatCreated()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ChannelChatCreated = true;
+            return update;
+        }
+
+        public static Update CreateMessageTypeMessageAutoDeleteTimerChanged()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.MessageAutoDeleteTimerChanged = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeMigrateToChatId()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.MigrateToChatId = 12345;
+            return update;
+        }
+
+        public static Update CreateMessageTypeMigrateFromChatId()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.MigrateFromChatId = 12345;
+            return update;
+        }
+
+        public static Update CreateMessageTypePinnedMessage()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.PinnedMessage = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeInvoice()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Invoice = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeSuccessfulPayment()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.SuccessfulPayment = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeUsersShared()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.UsersShared = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeChatShared()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ChatShared = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeConnectedWebsite()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ConnectedWebsite = "example.com";
+            return update;
+        }
+
+        public static Update CreateMessageTypeWriteAccessAllowed()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.WriteAccessAllowed = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypePassportData()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.PassportData = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeProximityAlertTriggered()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ProximityAlertTriggered = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeBoostAdded()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.BoostAdded = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeChatBackgroundSet()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ChatBackgroundSet = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeForumTopicCreated()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ForumTopicCreated = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeForumTopicEdited()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ForumTopicEdited = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeForumTopicClosed()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ForumTopicClosed = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeForumTopicReopened()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.ForumTopicReopened = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGeneralForumTopicHidden()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GeneralForumTopicHidden = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGeneralForumTopicUnhidden()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GeneralForumTopicUnhidden = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGiveawayCreated()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GiveawayCreated = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGiveaway()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.Giveaway = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGiveawayWinners()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GiveawayWinners = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeGiveawayCompleted()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.GiveawayCompleted = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideoChatScheduled()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.VideoChatScheduled = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideoChatStarted()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.VideoChatStarted = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideoChatEnded()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.VideoChatEnded = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeVideoChatParticipantsInvited()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.VideoChatParticipantsInvited = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeWebAppData()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.WebAppData = new();
+            return update;
+        }
+
+        public static Update CreateMessageTypeWebApp()
+        {
+            var update = CreateUpdateWithTypeMessage();
+            update.Message.WebAppData = new();
+            return update;
+        }
 
         #endregion
 
--- PRTelegramBot.Tests/EventsTests/MessageEventsTest.cs
+++ PRTelegramBot.Tests/EventsTests/MessageEventsTest.cs
@@ -24,7 +24,7 @@ namespace PRTelegramBot.Tests.EventsTests
         [Test]
         public async Task OnContactShouldBeInvoked()
         {
-            var update = UpdateSetUp.CreateUpdateTypeRemovedChatBoost();
+            var update = UpdateSetUp.CreateMessageTypeContact();
             bool eventCalled = false;
 
             Task EventHandler(BotEventArgs e)
@@ -33,10 +33,263 @@ namespace PRTelegramBot.Tests.EventsTests
                 return Task.CompletedTask;
             }
 
-            bot.Events.UpdateEvents.OnRemovedChatBoostHandle += EventHandler;
+            bot.Events.MessageEvents.OnContactHandle += EventHandler;
             await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
-            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.UpdateEvents.OnRemovedChatBoostHandle)} event was not called.");
-            bot.Events.UpdateEvents.OnRemovedChatBoostHandle -= EventHandler;
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnContactHandle)} event was not called.");
+            bot.Events.MessageEvents.OnContactHandle -= EventHandler;
         }
+
+        [Test]
+        public async Task OnPollShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypePoll();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnPollHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnPollHandle)} event was not called.");
+            bot.Events.MessageEvents.OnPollHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnLocationShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeLocation();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnLocationHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnLocationHandle)} event was not called.");
+            bot.Events.MessageEvents.OnLocationHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnWebAppsShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeWebApp();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnWebAppsHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnWebAppsHandle)} event was not called.");
+            bot.Events.MessageEvents.OnWebAppsHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnDocumentShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeDocument();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnDocumentHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnDocumentHandle)} event was not called.");
+            bot.Events.MessageEvents.OnDocumentHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnAudioShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeAudio();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnAudioHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnAudioHandle)} event was not called.");
+            bot.Events.MessageEvents.OnAudioHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnVideoShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeVideo();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnVideoHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnVideoHandle)} event was not called.");
+            bot.Events.MessageEvents.OnVideoHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnPhotoShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypePhoto();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnPhotoHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnPhotoHandle)} event was not called.");
+            bot.Events.MessageEvents.OnPhotoHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnStickerShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeSticker();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnStickerHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnStickerHandle)} event was not called.");
+            bot.Events.MessageEvents.OnStickerHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnVoiceShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeVoice();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnVoiceHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnVoiceHandle)} event was not called.");
+            bot.Events.MessageEvents.OnVoiceHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnVenueShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeVenue();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnVenueHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnVenueHandle)} event was not called.");
+            bot.Events.MessageEvents.OnVenueHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnGameShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeGame();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnGameHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnGameHandle)} event was not called.");
+            bot.Events.MessageEvents.OnGameHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnVideoNoteShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeVideoNote();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnVideoNoteHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnVideoNoteHandle)} event was not called.");
+            bot.Events.MessageEvents.OnVideoNoteHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnDiceShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeDice();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnDiceHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnDiceHandle)} event was not called.");
+            bot.Events.MessageEvents.OnDiceHandle -= EventHandler;
+        }
+
+        [Test]
+        public async Task OnAnimationShouldBeInvoked()
+        {
+            var update = UpdateSetUp.CreateMessageTypeAnimation();
+            bool eventCalled = false;
+
+            Task EventHandler(BotEventArgs e)
+            {
+                eventCalled = true;
+                return Task.CompletedTask;
+            }
+
+            bot.Events.MessageEvents.OnAnimationHandle += EventHandler;
+            await bot.Handler.HandleUpdateAsync(bot.botClient, update, new CancellationToken());
+            Assert.IsTrue(eventCalled, $"The {nameof(bot.Events.MessageEvents.OnAnimationHandle)} event was not called.");
+            bot.Events.MessageEvents.OnAnimationHandle -= EventHandler;
+        }
+
     }
 }
